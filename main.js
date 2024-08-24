f2df5a1321e9291f88c2d9143558c4b18325b0d1
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-08-24 16:48:21 +0100

    some stuff

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
 
 start_btn.onclick = () => {
   info_box.classList.add("activeInfo");
@@ -9,4 +10,9 @@ start_btn.onclick = () => {
 
 exit_btn.onclick = () => {
   info_box.classList.remove("activeInfo");
+};
+
+continue_btn.onclick = () => {
+  info_box.classList.remove("activeInfo");
+  quiz_box.classList.add("activeQuiz");
 };
