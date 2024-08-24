235d57f04e14dbe71f7abc87a404188424b87590
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-08-24 15:43:09 +0100

    some shii

---
+++ main.js
@@ -0,0 +1,12 @@
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+
+start_btn.onclick = () => {
+  info_box.classList.add("activeInfo");
+};
+
+exit_btn.onclick = () => {
+  info_box.classList.remove("activeInfo");
+};
