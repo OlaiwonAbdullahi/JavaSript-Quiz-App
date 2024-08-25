a4efcad58172ea0c60721cced9cc545dd70bab98
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-08-25 12:51:05 +0100

    added question counter

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const quiz_box = document.querySelector(".quiz_box");
 const myH1 = document.getElementById("myH1");
 let username = window.prompt("please what's your name");
 
-myH1.textContent = `Hey ${username}`;
+myH1.textContent = `👋Hey ${username}`;
 start_btn.onclick = () => {
   info_box.classList.add("activeInfo");
 };
@@ -19,16 +19,20 @@ continue_btn.onclick = () => {
   info_box.classList.remove("activeInfo");
   quiz_box.classList.add("activeQuiz");
   showQuestion(0);
+  queCounter(1);
 };
 
 let que_count = 0;
+let que_numb = 1;
 
 //next btn clicked
 const next_btn = quiz_box.querySelector(".next_btn");
 next_btn.onclick = () => {
   if (que_count < questions.length - 1) {
     que_count++;
+    que_numb++;
     showQuestion(que_count);
+    queCounter(que_numb);
   } else {
     console.log("question completed");
   }
@@ -62,4 +66,16 @@ function showQuestion(index) {
 
   que_text.innerHTML = que_tag;
   option_list.innerHTML = option_tag;
+}
+
+function queCounter(index) {
+  const bottom_ques_counter = quiz_box.querySelector(".total_que");
+  let totalQuesCountTag =
+    "<span><p>" +
+    index +
+    "</p> of <p>" +
+    questions.length +
+    "</p> questions</span>";
+
+  bottom_ques_counter.innerHTML = totalQuesCountTag;
 }
